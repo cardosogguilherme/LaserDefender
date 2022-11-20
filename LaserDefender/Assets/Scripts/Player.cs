e1fee6689df2e05cb27ef500c7e3c18aa99e59fa
Guilherme Cardoso <cardosogguilherme@gmail.com>
2022-11-20 21:01:42 +1300

    114. Boundaries

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -8,18 +8,41 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingBottom;
+
     Vector2 rawInput;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    private void Start()
+    {
+        InitBounds();
+    }
 
     void Update()
     {
         MovePlayer();
+    }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
     }
 
     private void MovePlayer()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
